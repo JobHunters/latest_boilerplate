aded2e2c92aa943fe0e98378f2aeb42ee2e8f6cc
Varonss <martin.varona.84@gmail.com>
2018-09-30 22:45:52 -0500

    Made App state full component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 //RHL only for front end development
+import { Container, Grid } from 'semantic-ui-react';
 import { hot } from 'react-hot-loader';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      openOrder: false,
+      total: 0,
+      sum: 0,
+      products: [],
+      cart: [],
+    }
+  }
   render() {
-    return <div>Welcome to a Modern Minimal React Boilerplate</div>;
+    return (
+      <Container className={style.root}>
+        <Grid>
+          <Grid.Column md-12='true' width={8}>
+          </Grid.Column>
+          <Grid.Column width={8}>
+          </Grid.Column>
+        </Grid>
+      </Container>
+    )
   }
 }
 //hot export works with RHL. Remove line 11 when starting fullstack integration
